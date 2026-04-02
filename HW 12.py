2cf587342ddfb77f90b3c268122d20debd34e255
Bilal Uzunkaya <bilaluzunkaya@gmail.com>
2026-04-02 12:33:24 +0300

    HW 12

---
+++ HW 12.py
@@ -0,0 +1,45 @@
+class Car:
+  def __init__(self, registration_number, maximum_speed):
+    self.registration_number = registration_number
+    self.maximum_speed = maximum_speed
+    self.current_speed = 0
+    self.odometer = 0
+
+  def accelerate(self, speed_change):
+    self.current_speed += speed_change
+
+    if self.current_speed > self.maximum_speed:
+      self.current_speed = self.maximum_speed
+
+    if self.current_speed < 0:
+      self.current_speed = 0
+
+  def drive(self, hours):
+    self.odometer += self.current_speed * hours
+
+
+class ElectricCar(Car):
+  def __init__(self, registration_number, maximum_speed, battery_capacity):
+    super().__init__(registration_number, maximum_speed)
+    self.battery_capacity = battery_capacity
+
+
+class GasolineCar(Car):
+  def __init__(self, registration_number, maximum_speed, tank_volume):
+    super().__init__(registration_number, maximum_speed)
+    self.tank_volume = tank_volume
+
+
+# Main program
+if __name__ == "__main__":
+  electric_car = ElectricCar("ABC-15", 180, 52.5)
+  gasoline_car = GasolineCar("ACD-123", 165, 32.3)
+
+  electric_car.accelerate(120)
+  gasoline_car.accelerate(100)
+
+  electric_car.drive(3)
+  gasoline_car.drive(3)
+
+  print(f"{electric_car.registration_number} odometer: {electric_car.odometer} km")
+  print(f"{gasoline_car.registration_number} odometer: {gasoline_car.odometer} km")
